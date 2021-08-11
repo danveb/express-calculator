38ffa6ab5f792b07b196b86ae5f136fc414a0908
Daniel Bae <danibae91@icloud.com>
2021-08-10 22:15:36 -0700

    move all functions to helpers.js file

--- app.js
+++ app.js
@@ -1,67 +1,9 @@
 // import express, ExpressError & app
 const express = require('express') 
+const { getNums, getMean, getMedian, getMode } = require('./helper') 
 const ExpressError = require('./expressError') 
 const app = express() 
 app.use(express.json())
-
-// function getNumber
-function getNums(numStr) {
-    if (numStr === undefined) {
-        throw new ExpressError('nums are required', 400)
-    }
-    let nums = numStr.split(',')
-    for(let i = 0; i < nums.length; i++) {
-        const num = nums[i]
-        nums[i] = Number(num)
-        if (isNaN(nums[i])) {
-            // not a number, throw an error
-            throw new ExpressError(`${num} is not a number`, 400)
-        }
-    }
-    return nums;
-}
-
-// find Mean
-function getMean(nums) {
-    let total = 0; 
-    nums.forEach(num => {
-        total += num;
-    })
-    return total/nums.length; 
-}
-
-// find Median
-function getMedian(nums) {
-    let median = 0, count = nums.length;
-    // sort array from low-to-high 
-    let sortedArr = nums.sort(function(a, b){return a-b})
-    if (count % 2 === 0) {  // is even
-        median = (sortedArr[count / 2 - 1] + sortedArr[count / 2]) / 2;
-    } else { // is odd
-        median = sortedArr[(count - 1) / 2];
-    }
-    return median;
-}
-
-// find Mode 
-function getMode(nums) {
-    let frequency = {} // array of frequency
-    let maxFreq = 0 // holds max frequency 
-    let mode = []
-
-    for(let i in nums) {
-        frequency[nums[i]] = (frequency[nums[i]] || 0) + 1; // increment frequency
-        if(frequency[nums[i]] > maxFreq) {
-            maxFreq = frequency[nums[i]]
-        }
-    }
-    for(let k in frequency) {
-        if(frequency[k] == maxFreq) {
-            mode.push(k)
-        }
-    }
-    return mode; 
-}
 
 // Route Handler /mean (average)
 app.get('/mean', function(req, res, next) {
@@ -109,4 +51,4 @@ app.use((err, req, res, next) => {
 // Listen for Port 3000 
 app.listen(3000, () => {
     console.log('Server running on port 3000') 
-})
+});
