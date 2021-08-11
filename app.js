a373c0495e50f593c709fb2bee413cd716c7aed0
Daniel Bae <danibae91@icloud.com>
2021-08-10 21:38:46 -0700

    add mode route

--- app.js
+++ app.js
@@ -35,13 +35,32 @@ function getMedian(nums) {
     let median = 0, count = nums.length;
     // sort array from low-to-high 
     let sortedArr = nums.sort(function(a, b){return a-b})
-    sortedArr.sort();
     if (count % 2 === 0) {  // is even
         median = (sortedArr[count / 2 - 1] + sortedArr[count / 2]) / 2;
     } else { // is odd
         median = sortedArr[(count - 1) / 2];
     }
     return median;
+}
+
+// find Mode 
+function getMode(nums) {
+    let frequency = {} // array of frequency
+    let maxFreq = 0 // holds max frequency 
+    let mode = []
+
+    for(let i in nums) {
+        frequency[nums[i]] = (frequency[nums[i]] || 0) + 1; // increment frequency
+        if(frequency[nums[i]] > maxFreq) {
+            maxFreq = frequency[nums[i]]
+        }
+    }
+    for(let k in frequency) {
+        if(frequency[k] == maxFreq) {
+            mode.push(k)
+        }
+    }
+    return mode; 
 }
 
 // Route Handler /mean (average)
@@ -66,6 +85,14 @@ app.get('/median', (req, res, next) => {
 })
 
 // Route Handler /mode (most frequent)
+app.get('/mode', (req, res, next) => {
+    let nums = getNums(req.query.nums)
+    let result = getMode(nums)
+    return res.json({
+        operation: "mode", 
+        value: result 
+    })
+})
 
 // Error Handler Default 
 app.use((err, req, res, next) => {
